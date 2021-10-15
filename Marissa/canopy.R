a4a693a31c80a53ed114fc08f3f31663d5eb4b0c
Alejandro Brambila <34696273+alebrambila@users.noreply.github.com>
2021-10-15 10:40:47 -0700

    test

--- Marissa/canopy.R
+++ Marissa/canopy.R
@@ -8,5 +8,9 @@ canopy<-read_csv("canopy_cover.csv")%>%
 
 ggplot(canopy, aes(x=orchard_age, y=canopy))+geom_boxplot(aes(group=orchard_age))+geom_jitter(aes(group=orchard_age))+facet_wrap(~year)
   
+
+plotkey<-canopy%>%
+  group_by(orchard_age, block, management, seedmix)%>%
+  summarize()
 #need to add in block 6 for 2020, and values for 2021
 # adjustment for lower half of 15 year old that should have a higher canopy due to xmas trees
